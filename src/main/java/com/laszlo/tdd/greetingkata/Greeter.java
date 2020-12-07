15dca497e5852bb09cd578ea8b1f7dacc02d75de
voidlaszlo <voidlaszlo@gmail.com>
2020-12-07 09:57:58 +0100

    implemented greeting-kata requirement 1-5

--- src/main/java/com/laszlo/tdd/greetingkata/Greeter.java
+++ src/main/java/com/laszlo/tdd/greetingkata/Greeter.java
@@ -1,9 +1,35 @@
 package com.laszlo.tdd.greetingkata;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Iterator;
+import java.util.List;
+
 public class Greeter {
 
+    public String greet() {
+        return "Hello, my friend.";
+    }
+
     public String greet(String name) {
-        return null;
+        if(StringUtils.isAllUpperCase(name)) {
+            return "HELLO " + name + "!";
+        }
+        return "Hello, " + name + ".";
+    }
+
+    public String greet(List<String> names) {
+        StringBuilder result = new StringBuilder().append("Hello");
+        Iterator<String> iterator = names.iterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            if (!iterator.hasNext()) {
+                result.append(" and " + name + ".");
+                break;
+            }
+            result.append(", " + name);
+        }
+        return result.toString();
     }
 
 }
--- src/test/java/com/laszlo/tdd/greetingkata/GreeterTest.java
+++ src/test/java/com/laszlo/tdd/greetingkata/GreeterTest.java
@@ -2,6 +2,8 @@ package com.laszlo.tdd.greetingkata;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreeterTest {
@@ -12,7 +14,7 @@ class GreeterTest {
     givenName_whenGreet_thenReturnsCorrectGreeting() {
         // GIVEN
         final String name = "John";
-        String expected = "Hello, " + name;
+        String expected = "Hello, " + name + ".";
 
         // WHEN
         String actual = greeter.greet(name);
@@ -20,5 +22,58 @@ class GreeterTest {
         // THEN
         assertEquals(expected, actual);
     }
+
+    @Test void
+    givenNoName_whenGreet_thenReturnsStandInGreeting() {
+        // GIVEN
+        final String name = null;
+        String expected = "Hello, my friend.";
+
+        // WHEN
+        String actual = greeter.greet();
+
+        // THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test void
+    givenAllUppercaseName_whenGreet_thenReturnsShoutedGreeting() {
+        // GIVEN
+        final String name = "JERRY";
+        String expected = "HELLO " + name + "!";
+
+        // WHEN
+        String actual = greeter.greet(name);
+
+        // THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test void
+    givenTwoNames_whenGreet_thenReturnsCorrectGreeting() {
+        // GIVEN
+        final List<String> names = List.of("Jill", "Jane");
+        String expected = "Hello, Jill and Jane.";
+
+        // WHEN
+        String actual = greeter.greet(names);
+
+        // THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test void
+    givenMoreThanTwoNames_whenGreet_thenReturnsCorrectGreeting() {
+        // GIVEN
+        final List<String> names = List.of("Amy", "Brian", "Charlotte");
+        String expected = "Hello, Amy, Brian and Charlotte.";
+
+        // WHEN
+        String actual = greeter.greet(names);
+
+        // THEN
+        assertEquals(expected, actual);
+    }
+
 
 }
